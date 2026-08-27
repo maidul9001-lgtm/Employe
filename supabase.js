1be1b33e2db894b828dafb2677fb7c6ab14802ae
maidul9001-lgtm <maidul9001@gmail.com>
2026-08-28 03:36:46 +0600

    Add files via upload

---
+++ supabase.js
@@ -0,0 +1,8 @@
+// Supabase Configuration
+// এই ফাইলে শুধু Project URL এবং Publishable Key থাকবে.
+// Secret / service_role key কখনো এখানে রাখবেন না.
+
+window.SUPABASE_URL = "https://hfhfwviyhugtmucvieic.supabase.co";
+
+window.SUPABASE_PUBLISHABLE_KEY =
+  "sb_publishable_-UhU99uy1RtlZ0MJ0hMsuA_-lhHivLT";
